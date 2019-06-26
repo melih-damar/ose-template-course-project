568d4be4d895b49cdae70ef1ec32efb25f3b0407
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2019-06-26 08:54:50 +0200

    edits

--- utils/travis_runner.py
+++ utils/travis_runner.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 """This script manages all tasks for the TRAVIS build server."""
 import subprocess as sp
+import glob
 
 if __name__ == '__main__':
 
-    notebook = 'student_project.ipynb'
-    cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
-    sp.check_call(cmd, shell=True)
+    for notebook in glob.glob('*.ipynb'):
+        cmd = ' jupyter nbconvert --execute {}  --ExecutePreprocessor.timeout=-1'.format(notebook)
+        sp.check_call(cmd, shell=True)
